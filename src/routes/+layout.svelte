0f016008c2fb6353f9125c969145f6c5f7229948
yuki-ks <yuki.ks.kp7@gmail.com>
2024-03-24 12:23:24 +0900

    Switch sveltekit skeletonui apps

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.pcss';
+  import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+  <svelte:fragment slot="header">
+  </svelte:fragment>
+  <svelte:fragment slot="sidebarLeft">
+  </svelte:fragment>
+  <svelte:fragment slot="sidebarRight">
+  </svelte:fragment>
+  <slot />
+</AppShell>
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+  darkMode: 'class',
+	content: [
+    './src/**/*.{html,js,svelte,ts}',
+    join(
+      require.resolve(
+        '@skeletonlabs/skeleton',
+        '../**/*.{html,js,svelte,ts}'
+      )
+    )
+  ],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [
+    skeleton
+  ]
+};
+
+module.exports = config;
